0e415e055f4d05c1184283757976780ccffa9c60
tecnicfitia-TUTORIAL <tecnicfitia@tecnicalfitnesartificialintelligence.app>
2025-12-02 18:40:12 +0000

    biometrico2

--- services/firebaseConfig.ts
+++ services/firebaseConfig.ts
@@ -21,7 +21,7 @@ let analytics: any = null;
 
 const firebaseConfig = {
   apiKey: env.VITE_FIREBASE_API_KEY,
-  authDomain: env.VITE_FIREBASE_AUTH_DOMAIN,
+  authDomain: env.VITE_FIREBASE_AUTH_DOMAIN || (env.VITE_FIREBASE_PROJECT_ID ? `${env.VITE_FIREBASE_PROJECT_ID}.firebaseapp.com` : undefined),
   projectId: env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: env.VITE_FIREBASE_MESSAGING_SENDER_ID,
